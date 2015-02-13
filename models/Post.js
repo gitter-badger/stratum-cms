1419c06a54048e7d52de392b9b7b4fce110b7bf5
Fredrik Lundberg <fredrik@ablu.se>
2015-02-13 16:10:03 +0100

    Changed the brief content to simple text and added croping to carousel

--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,7 @@ Post.add({
 	subtitle: { type: String },
 	image: { type: Types.CloudinaryImage },
 	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		brief: { type: Types.Textarea, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 }
 	},
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true }
