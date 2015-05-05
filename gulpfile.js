9321ecbb6241b5aaf29dfc25450bc8cc56982f10
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-05-05 11:11:12 +0200

    ESLint
    Changed from jshint to ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	jshintReporter = require('jshint-stylish'),
+	eslint = require('gulp-eslint'),
 	watch = require('gulp-watch');
 
 /*
  * Create variables for our project paths so we can change in one place
  */
 var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+	'src':['./models/**/*.js', './routes/**/*.js', 'keystone.js']
 };
 
 
 // gulp lint
 gulp.task('lint', function(){
 	gulp.src(paths.src)
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshintReporter));
+		.pipe(eslint())
+		.pipe(eslint.format());
+		// .pipe(eslint.failOnError());
+		// .pipe(jshint.reporter(jshintReporter));
 
 });
 
@@ -23,6 +26,6 @@ gulp.task('lint', function(){
 gulp.task('watch:lint', function () {
 	gulp.src(paths.src)
 		.pipe(watch())
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshintReporter));
+		.pipe(eslint())
+		.pipe(eslint.format());
 });
