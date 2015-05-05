9f7d02c011c41b0e7c1ddbb65436df59f09b5c14
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-05-05 13:04:04 +0200

    Removed JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	jshintReporter = require('jshint-stylish'),
 	eslint = require('gulp-eslint'),
 	watch = require('gulp-watch');
 
@@ -17,9 +15,6 @@ gulp.task('lint', function(){
 	gulp.src(paths.src)
 		.pipe(eslint())
 		.pipe(eslint.format());
-		// .pipe(eslint.failOnError());
-		// .pipe(jshint.reporter(jshintReporter));
-
 });
 
 // gulp watcher for lint
