0f9ec402f058ed56ae6072528fdbb8abb478e285
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-05-07 12:47:38 +0200

    Gulp task for recycling the iis node app
    Added a watch task to enable recycling of app pool when backend changes
    has been made

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	eslint = require('gulp-eslint'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	iis = require('IIS');
 
+require('dotenv').load();
 /*
  * Create variables for our project paths so we can change in one place
  */
@@ -23,4 +25,23 @@ gulp.task('watch:lint', function () {
 		.pipe(watch())
 		.pipe(eslint())
 		.pipe(eslint.format());
+});
+
+// gulp watcher for recycling the app pool running keystone on iisnode
+gulp.task('watch:iis', function(){
+	var appPool = process.env.IIS_APP_POOL;
+	if(!appPool){
+		console.log('IIS_APP_POOL is not defined in .env');
+		return;
+	}
+	gulp.watch(paths.src)
+	.on('change', function(file){
+		console.log('file changed: ' + file.path);
+		iis.recycleAppPool(appPool, function(err, output){
+			console.log(output.toString());
+			if(err){
+				console.log(err);
+			}
+		});
+	});
 });
