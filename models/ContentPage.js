91b813c8b070787abbfddc545ecbb7c45a128012
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-05-21 17:32:47 +0200

    Working prototype of template style image insertion
    Work in progress, currently possible to add images found in "images"
    field by writing the following syntax in markdown ${bild[0-9]+}

--- models/ContentPage.js
+++ models/ContentPage.js
@@ -20,12 +20,39 @@ ContentPage.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	subtitle: { type: String },
 	image: { type: Types.CloudinaryImage },
+	images: { note: 'Add images which should be placed inline.', type: Types.CloudinaryImages },
 	content: {
 		brief: { type: Types.Textarea, height: 150, hidden: true },
 		extended: { type: Types.Html, wysiwyg: true, height: 400, hidden: true },
 		markdown: { label: 'Content Markdown', type: Types.Markdown, height: 400, toolbarOptions: { hiddenButtons: 'H1,H6,Code' } },
 		wysiwyg: { type: Types.Html, height: 400, wysiwyg: true }
 	}
+});
+
+//TODO: Exception handling!
+//TODO: Cloudinary url
+ContentPage.schema.pre('save', function(next){
+	var imageRegEx = /\$\{bild([0-9]+)\}/g,
+		matches = imageRegEx.exec(this.content.markdown.md),
+		newMd = '',
+		nextSub = 0,
+		images = this.images.length,
+		imageArr = [], i;
+		while(matches){
+			i = parseInt(matches[1], 10) - 1;
+			if(i < images){
+				imageArr.push(this.images[i].url);
+				newMd += this.content.markdown.md.substr(nextSub, imageRegEx.lastIndex - nextSub - matches[0].length);
+				newMd += '![desc](' + this.images[i].url + ' "title")';
+				nextSub = imageRegEx.lastIndex;
+			}
+			matches = imageRegEx.exec(this.content.markdown.md);
+		}
+	newMd += this.content.markdown.md.substr(nextSub);
+	// console.log(newMd);
+	// console.log(imageArr);
+	this.content.markdown.md = newMd;
+	next();
 });
 
 ContentPage.schema.virtual('content.full').get(function() {
